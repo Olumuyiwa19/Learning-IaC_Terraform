d7cf9b20a4cc5c7c0bc84919744812dab43b6cd1
Olumuyiwa19 <kolayemi.olumuyiwa@gmail.com>
2022-01-13 21:47:29 +0000

    changes the underscore to hyphen to align with s3 naming convention

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 #1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state_s3" {
   #make sure you give unique bucket name
-  bucket        = "learning-terraform2022_v2-state"
+  bucket        = "learning-terraform2022-v2-state"
   force_destroy = true
   # Enable versioning to see full revision history of our state files
   versioning {
